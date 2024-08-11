2021609f16a9cdd27cf8bc003f495bb76c220c1d
rosafael <rafael300120@gmail.com>
2024-08-11 18:51:45 -0300

    da

--- Assets/TerrorXande/MemorieObj.cs
+++
@@ -1,9 +0,0 @@
-public class MemorieObj : InterectableObj
-{
-    protected override void Interact()
-    {
-        MemoriesCounter.Instance.memoriesCount++;
-        PlayerInteract.Instance.OnInteractionEffected.Invoke();
-        Destroy(gameObject);
-    }
-}
